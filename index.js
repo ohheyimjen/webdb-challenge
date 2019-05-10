10ca7da541a6ec0fcb365f7f99e6b4f539204247
ohheyimjen <jendority@gmail.com>
2019-05-10 08:08:00 -0700

    created index.js file

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const helmet = require('helmet');
+const knex = require('knex');
+
+const knexConfig = require('./knexfile.js')
+const db = knex(knexConfig.development);
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+
+const port = process.env.PORT || 6000;
+server.listen(port, () =>
+  console.log(`\n** API running on http://localhost:${port} **\n`)
+);
