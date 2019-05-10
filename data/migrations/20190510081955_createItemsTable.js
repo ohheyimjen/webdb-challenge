5fae087895ee87d3ef43cb118f40f75f50e6429f
ohheyimjen <jendority@gmail.com>
2019-05-10 09:02:43 -0700

    successfully linked db and tables to sqliteStudio

---
+++ data/migrations/20190510081955_createItemsTable.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('items', tbl => {
+    tbl.increments();
+
+    tbl
+      .string('name', 128).notNullable()
+      .string('description', 250).notNullable()
+      .string('notes', 250)
+      .unique()
+
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('items');
+};
--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,12 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './data/sprint.db3'
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    useNullAsDefault: true
   },
 
   
